d55663f7540c352a355f3ab4af930f1a334a103e
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-12 20:52:57 +0100

    Update insert.js

--- src/utils/insert.js
+++ src/utils/insert.js
@@ -10,7 +10,7 @@ export class Insert {
     constructor(tableName, values) {
         console.log(tableName)
         this.tableName = tableName;
-        this.setValues(fields);
+        this.setValues(values);
     }
     
     setValues(values){
