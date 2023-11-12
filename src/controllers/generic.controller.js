0610f8fddfc7107239aa7ae9056db24f73fbaa11
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-12 20:51:36 +0100

    Update generic.controller.js

--- src/controllers/generic.controller.js
+++ src/controllers/generic.controller.js
@@ -1,6 +1,7 @@
 import { v4 } from "uuid";
 import { getConnection } from "../database.js";
-import { Query,Insert } from "../utils/query.js";
+import { Query } from "../utils/query.js";
+import { Insert } from "../utils/insert.js";
 
 export const getTableItem = async (req, res) => {
     const table = req.params.table
