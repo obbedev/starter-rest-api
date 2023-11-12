f66ef7edff9bbcfc86ba895ac11921b2e9c6f371
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-12 20:47:59 +0100

    Update table.js

--- src/routes/table.js
+++ src/routes/table.js
@@ -1,10 +1,12 @@
 import router from "./router.js";
 import {
   getTableItem,
-  getTableItems
+  getTableItems,
+  insert
 } from "../controllers/generic.controller.js";
 
 router.get("/:table", getTableItems);
 router.get("/:table/:id", getTableItem);
+router.post("/:table/", insert);
 
 export default router;
