6bba57424af0d01a61e0763049d51e2416e2b280
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-13 22:16:52 +0100

    Create update.js

---
+++ src/utils/update.js
@@ -0,0 +1,38 @@
+export class Update {
+    tableName = '';
+    values = [];
+    filter = "";
+    filters = [];
+    constructor(tableName, values) {
+        console.log(tableName)
+        this.tableName = tableName;
+        this.setValues(values);
+    }
+    
+    setValues(values){
+        this.values = values;
+    }
+
+    setFilter(filter){
+        this.filter = filter;
+    }
+
+    addFilter(filter){
+      this.filters.push(filter);
+    }
+
+    toString() {
+        //update event set name = '',id='' where id = XXX
+        let query = "UPDATE "+this.tableName+" SET ";
+        let result = Object.keys(this.values).map(item => {
+          return `${item} = '${this.values[item]}'`
+        });
+        query += " "+result.join(",")+")";
+        if(this.filter){
+          
+        }
+        console.log(query);
+        return query;
+    }
+
+}
