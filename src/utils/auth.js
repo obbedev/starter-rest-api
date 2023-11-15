87cba15d034b2b79ebf92216d11f885f546e5963
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-15 21:37:16 +0100

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,16 +4,11 @@ import { getConnection } from "../database.js";
 export const isLogged = async (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ')[1];
-      console.log("current request token",token);
       if(token && (typeof token === 'string' || token instanceof String)){
         let query = new Query('api_user','id');
         query.setFilter("token = '"+token+"'");
         const db = getConnection();
-        await db.query("select id from api_user", (error, results) => {
-           res.status(200).json({
-                results
-            });
-          console.log("current request response",results);
+        await db.query(query.toString(), (error, results) => {
           if(results.rows.length>0){
             next();
           }else{
