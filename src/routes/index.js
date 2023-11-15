f0968c3b73bc175dd1ee1c350c1e5e62b44f3310
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-15 21:51:32 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import taskRoutes from "./tasks.js";
 import tableRoutes from "./table.js";
+import authRoutes from "./table.js";
 import express from "express";
 const app = express();
 import router from "./router.js";
  
 app.use(taskRoutes);
 app.use(tableRoutes);
-
+app.use(authRoutes);
 
 export default router;
