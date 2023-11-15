9f947582bba2f4f7faf2c3ecf2b3a9400fa1fb25
obbedev <136840210+obbedev@users.noreply.github.com>
2023-11-15 20:37:28 +0100

    Update table.js

--- src/routes/table.js
+++ src/routes/table.js
@@ -5,7 +5,9 @@ import {
   insert,
   update
 } from "../controllers/generic.controller.js";
+import { isLogged  } from "../utils/auth.js";
 
+router.get("/:table/test",isLogged,getTableItems);
 router.get("/:table", getTableItems);
 router.get("/:table/:id", getTableItem);
 router.post("/:table", insert);
