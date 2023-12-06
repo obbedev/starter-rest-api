807da1fa13bf85a3bb42fa42e233f7efd5c737bd
obbedev <obbedev@gmail.com>
2023-12-06 12:28:53 +0100

    improve fields

--- dist/src/database/fields.js
+++ dist/src/database/fields.js
@@ -1,10 +1,18 @@
 export class Fields {
-    constructor(fields) {
+    constructor(fields, allIfNone = false) {
         this.fields = [];
+        this.allIfNone = false;
         this.fields = fields;
+        this.allIfNone = allIfNone;
+    }
+    addField(field) {
+        this.fields.push(field);
     }
     generateFields() {
         let query = '';
+        if (!this.fields && this.allIfNone) {
+            return "*";
+        }
         if (Array.isArray(this.fields)) {
             //TODO could be subselect,if,...
             query += this.fields.map(item => {
--- src/database/fields.ts
+++ src/database/fields.ts
@@ -1,11 +1,20 @@
 export class Fields {
     private fields = [];
-    constructor(fields) {
+    private allIfNone = false;
+    constructor(fields,allIfNone = false) {
         this.fields = fields;
+        this.allIfNone = allIfNone;
+    }
+
+    addField(field){
+        this.fields.push(field);
     }
 
     generateFields() {
         let query = '';
+        if(!this.fields && this.allIfNone){
+            return "*";
+        }
         if (Array.isArray(this.fields)) {
             //TODO could be subselect,if,...
             query += this.fields.map(item => {
